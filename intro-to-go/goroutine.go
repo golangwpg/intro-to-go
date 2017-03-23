0120850fa2e66c919da186ae1d7e26e58263f000
andrewhoff <andrew@getkoble.com>
2017-03-23 01:43:02 -0500

    this too

--- intro-to-go/goroutine.go
+++ intro-to-go/goroutine.go
@@ -19,7 +19,6 @@ func fetch(uri string) error { // HL
 func main() { // HL
 	go fetch("https://www.google.com") // HL
 
-	// NOTE: we might want to do this with a channel? Or would that be too many things at once?
 	time.Sleep(2000)
 	fmt.Println("done")
 } // HL
