d883ea91e0a9e5c43f31c29f1063944cf6ed5cf2
andrewhoff <andrew@getkoble.com>
2017-03-23 01:38:31 -0500

    more things, Mark to review

--- intro-to-go/maps2.go
+++ intro-to-go/maps2.go
@@ -1,19 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
 	ageMap := map[string]int{"Mark": 43, "Andrew": 26} // HL
-
+	log.Println(ageMap)
+	
 	// update Andrew
 	ageMap["Andrew"] = 25 // HL
-
 	// remove Mark
 	delete(ageMap, "Mark") // HL
-
 	// add Eric
 	ageMap["Eric"] = 34 // HL
-
+	
 	for k, v := range ageMap { // HL
 		fmt.Printf("%s is %d years old\n", k, v)
 	} // HL
